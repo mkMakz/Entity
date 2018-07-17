5cd8f35c6c8a15d04778ca806e1d06679aba6a44
ОстроуховМ <@348_4>
2018-07-17 21:49:57 +0600

    Add project files.

---
+++ ADEnt/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ADEnt.Model;
+
+namespace ADEnt
+{
+    class Program
+    {
+        private static MCSEntities db = new MCSEntities();
+
+        static void Main(string[] args)
+        {
+            db.Database.Log = Console.Write;
+            //Явная загрузка
+            TablesModel model = db.TablesModel.Find(1);
+
+            db.Entry(model).Collection(c => c.TablesSNPrefix).Load();
+
+
+
+
+
+
+            return;
+
+
+            //Lazy load 
+            foreach (var tablesModel in db.TablesModel.Take(5))
+            {
+                Console.WriteLine(tablesModel.strName);
+                foreach (var tablesSnPrefix in tablesModel.TablesSNPrefix)
+                {
+                    Console.WriteLine(" -"+tablesSnPrefix.strPrefix);
+                }
+            }
+
+
+
+            return;
+
+
+            //Прямая загрузка
+            List<newEquipment> equipList = db.newEquipment
+                //.Include(d => d.TablesLocation)
+                //.Include(d => d.TablesModel)
+                //.Include(d => d.TablesModel.TablesSNPrefix)
+                .ToList();
+
+            Console.WriteLine("------------------------------------------------");
+            foreach (var newEquipment in equipList)
+            {
+                Console.WriteLine(newEquipment.intGarageRoom);
+                foreach (var pref in newEquipment.TablesModel.TablesSNPrefix)
+                {
+                    //Console.WriteLine("->" + pref.strPrefix);
+                }
+            }
+            Console.WriteLine("--------------------------------------------------");
+
+        }
+    }
+}
